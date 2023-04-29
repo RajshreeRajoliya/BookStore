b75d7bd0a430828f970d9c022b3d7ba5e3e63a70
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-04-29 22:15:57 +0530

    deleteBook

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -8,5 +8,6 @@ router.get("/", booksController.getAllBooks);
 router.post("/", booksController.addBook);
 router.get("/:id", booksController.getById);
 router.put("/:id", booksController.updateBook);
+router.delete("/:id", booksController.deleteBook);
 
 module.exports = router;
