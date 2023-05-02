80588ec8a920a74074f9a43ed4db493a88969e49
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-02 14:08:18 +0530

    Finished

--- book-store/src/components/Header.js
+++ book-store/src/components/Header.js
@@ -1,37 +1,33 @@
-
-import React from 'react'
-import { useState } from "react";
-import {AppBar,Typography,Toolbar,Tabs,Tab} from '@mui/material'
+import React, { useState } from "react";
+import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
 import { NavLink } from "react-router-dom";
-
 const Header = () => {
-const [value, setValue] = useState();
+  const [value, setValue] = useState();
   return (
     <div>
       <AppBar sx={{ backgroundColor: "#232F3D" }} position="sticky">
-      <Toolbar>
-      <Typography>
-      <LibraryBooksOutlinedIcon />
-      </Typography>
-
-      <Tabs  sx={{ ml: "auto" }}
+        <Toolbar>
+          <NavLink to="/" style={{ color: "white" }}>
+            <Typography>
+              <LibraryBooksOutlinedIcon />
+            </Typography>
+          </NavLink>
+          <Tabs
+            sx={{ ml: "auto" }}
             textColor="inherit"
             indicatorColor="primary"
             value={value}
             onChange={(e, val) => setValue(val)}
-            >
-
-<Tab LinkComponent={NavLink} to="/add" label="Add product" />
+          >
+            <Tab LinkComponent={NavLink} to="/add" label="Add product" />
             <Tab LinkComponent={NavLink} to="/books" label="Books" />
             <Tab LinkComponent={NavLink} to="/about" label="About Us" />
-
-      </Tabs>
-      </Toolbar>
-      
-      </AppBar> 
+          </Tabs>
+        </Toolbar>
+      </AppBar>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
